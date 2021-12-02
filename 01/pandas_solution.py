9b629e1e936542a05cc9e517cc4f5ce8d8532b3d
HoroTW <horo@horotw.com>
2021-12-02 12:10:23 +0100

    👌 IMPROVE: Remove dropna since query already does that

--- 01/pandas_solution.py
+++ 01/pandas_solution.py
@@ -2,8 +2,8 @@
 import pandas as pd
 
 df: pd.DataFrame = pd.read_csv("input.txt", header=None, names=["A"])
-print("Answer 1: ", df.diff().query("A > 0").dropna().count()[0])
-print("Answer 2: ", df.rolling(window=3).sum().diff().query("A > 0").dropna().count()[0])
+print("Answer 1: ", len(df.diff().query("A > 0")))
+print("Answer 2: ", len(df.rolling(window=3).sum().diff().query("A > 0")))
 
 # Even better solution since we don't really need a rolling window for the diff:
-print("Smarter Answer 2: ", df.diff(3).query("A > 0").dropna().count()[0])
+print("Smarter Answer 2: ", len(df.diff(3).query("A > 0")))
