e6e25177af760cf473d0d02077ec4ca969df288a
HoroTW <horo@horotw.com>
2021-12-08 19:37:36 +0100

    🐛 FIX: Not all needed positions where evaluated

--- 07/solution.py
+++ 07/solution.py
@@ -1,17 +1,16 @@
-positions = []
+def triangular_number(n):  # sum of the first n natural numbers has an explicit formula
+    return n * (n + 1) // 2  # == sum(range(n + 1)) == 1 + 2 + 3 + ... + n
+    # we can use the floor division operator `//` since a factor of 2 is always in n(n+1)
+    # since either n or n+1 is even (so it contains an factor of 2) --> always an integer
+
+
 with open("input.txt", encoding="ASCII") as f:
     positions = [int(x) for x in f.read().split(",")]
 
-
-def triangular_number(n): # the sum of the first n natural numbers has an cool explicit formula so we can use that :)
-    return n * (n + 1) // 2  # == sum(range(n + 1)) == 1 + 2 + 3 + ... + n
-    # we can use the floor division operator `//` since a factor of 2 is always in n(n+1)
-    # since either n or n+1 is even (so it contains an factor of 2) --> by /2 we will get an Integer anyway
-
-
-fuel = [0] * len(positions)
-triangular_fuel = [0] * len(positions)
-for i in range(len(positions)):
+max_position = max(positions) + 1
+fuel = [0] * max_position
+triangular_fuel = [0] * max_position
+for i in range(max_position):
     for position in positions:
         distance = abs(position - i)
         fuel[i] += distance
