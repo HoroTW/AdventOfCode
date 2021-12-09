993143a8a00b57b366ab18c3cf00320b43287c71
HoroTW <horo@horotw.com>
2021-12-10 00:22:12 +0100

    👌 IMPROVE: change solution to `scipy` based variant

--- 09/solution.py
+++ 09/solution.py
@@ -1,23 +1,17 @@
-# %%
-with open("input.txt", encoding="ascii") as f:
-    map2d = [list(map(int, list(x))) for x in f.read().splitlines()]
+import numpy as np
+from scipy.ndimage import generic_filter, label
 
-
-def neighbors_pos(row_pos, col_pos, map2d):
-    for row_step, col_step in ((1, 0), (-1, 0), (0, 1), (0, -1)):
-        n_row, n_col = (row_pos + row_step, col_pos + col_step)
-        if 0 <= n_row < len(map2d) and 0 <= n_col < len(map2d[0]):
-            yield (n_row, n_col)
-
-
-map_height, map_width = len(map2d), len(map2d[0])
-local_minimas = []
-
-for i_r, row in enumerate(map2d):
-    for i_c, cell in enumerate(row):
-        if all(map2d[n_row][n_col] > cell for n_row, n_col in neighbors_pos(i_r, i_c, map2d)):
-            local_minimas.append((i_r, i_c))
-
-risk = [map2d[minima[0]][minima[1]] + 1 for minima in local_minimas]
-print("Answer 1:", sum(risk))
-# %%
+map2d: np.ndarray = np.genfromtxt("input.txt", delimiter=1, dtype=int)
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.ndimage.generic_filter.html
+mask: np.ndarray = generic_filter(
+    map2d,
+    lambda x: min(x[:2]) > x[2] < min(x[3:]),
+    footprint=[  # shape of the window
+        [0, 1, 0],  # indexes are   [., 0, .],
+        [1, 1, 1],  #               [1, 2, 3],
+        [0, 1, 0],  #               [., 4, .],
+    ],
+    mode="constant",  # means that at the edges the cval value is used to pad around the input
+    cval=9,
+)
+print("Answer 1:", ((map2d + 1) * mask).sum())
