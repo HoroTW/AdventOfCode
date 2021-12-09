a227cd323f6ca92fb047fa8dddd4308dc8ca1b27
HoroTW <horo@horotw.com>
2021-12-09 16:19:40 +0100

    👌 IMPROVE: Simplify return using yield

--- 09/solution.py
+++ 09/solution.py
@@ -6,13 +6,10 @@ with open("input.txt", encoding="ascii") as f:
 
 
 def neighbor_positions(row_pos, col_pos, max_w, max_h):
-    ret_list = []
     for row_step, col_step in ((1, 0), (-1, 0), (0, 1), (0, -1)):
         n_row, n_col = (row_pos + row_step, col_pos + col_step)
         if 0 <= n_row < max_w and 0 <= n_col < max_h:
-            ret_list.append((n_row, n_col))
-    return ret_list
-
+            yield (n_row, n_col)
 
 
 # %%
